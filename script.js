df13e16fc2c7a5f1e505a650d4f6ce0a8a2dcc6e
Pav85 <pawel.werbowy@gmail.com>
2023-07-04 19:08:11 +0100

    background color update
    color

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ console.log("Starting generator!");
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+body.style.background = color1.value;
 
 color1.addEventListener("input", function () {
   console.log(color1.value);
